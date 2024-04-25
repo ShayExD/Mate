8981349a16781fc2cfac1ebf0eea9e6da1e55451
ShayExD <104307803+ShayExD@users.noreply.github.com>
2024-04-25 21:11:25 +0300

    Update dialogAi.jsx

--- MateClientSide/src/components/DialogAi/dialogAi.jsx
+++ MateClientSide/src/components/DialogAi/dialogAi.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { ScrollView } from 'react-native';
+import { ScrollView ,StyleSheet} from 'react-native';
 import { Dialog, Portal, Text } from 'react-native-paper';
 import Theme from '../../../assets/styles/theme';
 
@@ -7,15 +7,26 @@ const DialogAi = (props) => {
 
   return (
     <Portal>
-      <Dialog visible={props.visible} onDismiss={props.onDismiss}>
+      <Dialog visible={props.visible} onDismiss={props.onDismiss} >
         <Dialog.ScrollArea>
-          <ScrollView contentContainerStyle={{paddingHorizontal: 24}}>
-            <Text style={Theme.primaryText}>{props.Title}</Text>
+          <ScrollView contentContainerStyle={{alignItems: 'center'}}>
+            <Text style={[Theme.primaryText]}>{props.Title}</Text>
           </ScrollView>
         </Dialog.ScrollArea>
       </Dialog>
     </Portal>
   );
 };
+
+const styles = StyleSheet.create({
+    text: {
+        textAlign:'right'
+    },
+    check:{
+    }
+   
+  });
+
+
 
 export default DialogAi;
