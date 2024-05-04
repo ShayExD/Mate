3c4960556d06aa86f7cbe919c984425b0b6943d6
barleviatias <barlevi.atias.com>
2024-05-04 17:01:23 +0300

    fix the app js

--- MateClientSide/App.js
+++ MateClientSide/App.js
@@ -1,27 +1,32 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import { useContext } from 'react';
 import { StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { AuthProvider, AuthContext } from './AuthContext';
+import { AuthProvider,AuthContext } from './AuthContext';
 import { Provider as PaperProvider } from 'react-native-paper';
 import MainNavigation from './src/navigation/Navigation';
 import Tabs from './src/navigation/tabs';
 
-export default function App() {
-
-  const { loggedInUser} = useContext(AuthContext);
-
+const AppContent = () => {
+  const { loggedInUser } = useContext(AuthContext);
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-    <AuthProvider>
-    <NavigationContainer style={styles.container}>
-    <PaperProvider>
-            {loggedInUser ? <MainNavigation /> : <Tabs />}
-    </PaperProvider>
-    </NavigationContainer>
-    </AuthProvider>
+      <NavigationContainer style={styles.container}>
+        <PaperProvider>
+          {loggedInUser===null ? <MainNavigation /> : <Tabs />}
+        </PaperProvider>
+      </NavigationContainer>
     </TouchableWithoutFeedback>
-  )
+  );
+};
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <AppContent />
+    </AuthProvider>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -33,4 +38,4 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
-})
+});
